06881e22366bb5f020498cce1b39a8c1f71f751d
Kevin Rutherford <kevin@rutherford-software.com>
2016-08-18 20:44:55 +0100

    Display a welcome message and initial prompt

--- java/src/Application.java
+++ java/src/Application.java
@@ -5,7 +5,11 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Welcome to our little corner shop!");
+		System.out.println();
+		System.out.println("For help, type 'h' or 'help' or '?'");
+		System.out.println();
+		System.out.print("shop> ");
 	}
 
 }
