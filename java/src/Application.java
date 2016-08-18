4ec3e7cc1f2485c89e22e4a61642dd634b1b3bc9
Kevin Rutherford <kevin@rutherford-software.com>
2016-08-18 21:24:54 +0100

    Name some methods

--- java/src/Application.java
+++ java/src/Application.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class Application {
 
@@ -8,11 +10,13 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Welcome to our little corner shop!");
-		System.out.println();
-		System.out.println("For help, type 'h' or 'help' or '?'");
-		System.out.println();
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		displayWelcomeMessage(System.out);
+		commandLoop(System.in);
+		System.exit(0);
+	}
+
+	private static void commandLoop(InputStream input) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(input));
 		for (;;) {
 			System.out.print("shop> ");
 			try {
@@ -23,7 +27,13 @@ public class Application {
 			}
 		}
 		System.out.println();
-		System.exit(0);
+	}
+
+	private static void displayWelcomeMessage(PrintStream out) {
+		out.println("Welcome to our little corner shop!");
+		out.println();
+		out.println("For help, type 'h' or 'help' or '?'");
+		out.println();
 	}
 
 }
