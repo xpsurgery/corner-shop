1463160b8b085d2378c29ac4d826643c95232350
Kevin Rutherford <kevin@rutherford-software.com>
2016-08-19 12:10:24 +0100

    Sketch a basic set of commands

--- java/src/Application.java
+++ java/src/Application.java
@@ -23,10 +23,24 @@ public class Application {
 				String command = in.readLine();
 				if (command == null || command.equals("q"))
 					break;
+				if (command.equals("h"))
+					printHelp(System.out);
 			} catch (IOException e) {
 			}
 		}
 		System.out.println();
+	}
+
+	private static void printHelp(PrintStream out) {
+		out.println("b sku [num]   Add SKU to basket; num defaults to 1");
+		out.println("c             Checkout");
+		out.println("d sku         Show detailed product info for SKU");
+		out.println("h             Show this help");
+		out.println("l             List basket contents");
+		out.println("p [filter]    List products (matching filter)");
+		out.println("q             Quit");
+		out.println("r sku num     Replenish SKU with num additional units");
+		out.println("s             Show stock levels");
 	}
 
 	private static void displayWelcomeMessage(PrintStream out) {
