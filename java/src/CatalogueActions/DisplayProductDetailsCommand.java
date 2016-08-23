9c1d9dc62f604562e91836479bdf76dfa6a741e9
Kevin Rutherford <kevin@rutherford-software.com>
2016-08-23 20:43:59 +0100

    Show the price in the product detail view

--- java/src/CatalogueActions/DisplayProductDetailsCommand.java
+++ java/src/CatalogueActions/DisplayProductDetailsCommand.java
@@ -23,7 +23,7 @@ class DisplayProductDetailsCommand implements UserCommand {
 		if (sku == null)
 			System.err.println("ERROR: product code " + id + " not found");
 		else {
-			System.out.printf("%s\t%s\n\n", sku.id, sku.title);
+			System.out.printf("%s\t%s\t£%5.02f\n\n", sku.id, sku.title, sku.price / 100.0);
 			for (String line : sku.description())
 				System.out.println(line);
 		}
