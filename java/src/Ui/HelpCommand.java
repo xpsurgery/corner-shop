9de192da0046ddcba3a699daae070ee15dd9917f
Kevin Rutherford <kevin@rutherford-software.com>
2016-08-25 09:41:55 +0100

    Organise the help by user role

--- java/src/Ui/HelpCommand.java
+++ java/src/Ui/HelpCommand.java
@@ -11,15 +11,20 @@ class HelpCommand implements UserCommand {
 	}
 
 	public void run(UserInput cmd) {
-		out.println("a sku [num]   Add SKU to basket; num defaults to 1");
-		out.println("b             List basket contents");
-		out.println("c             Checkout");
-		out.println("d sku         Show detailed product info for SKU");
-		out.println("h             Show this help");
-		out.println("p [filter]    List products (matching filter)");
-		out.println("q             Quit");
-		out.println("r sku num     Replenish SKU with num additional units");
-		out.println("s             Show stock levels");
+		out.println("Customer actions:");
+		out.println("  a sku [num]   Add SKU to basket; num defaults to 1");
+		out.println("  b             List basket contents");
+		out.println("  c             Checkout");
+		out.println("  d sku         Show detailed product info for SKU");
+		out.println("  p [filter]    List products (matching filter)");
+		out.println();
+		out.println("Warehouse actions:");
+		out.println("  r sku num     Replenish SKU with num additional units");
+		out.println("  s             Show stock levels");
+		out.println();
+		out.println("General:");
+		out.println("  h             Show this help");
+		out.println("  q             Quit");
 	}
 
 }
