066f0d547554fdbe80e9510a3299345010e7b307
Kevin Rutherford <kevin@rutherford-software.com>
2016-09-06 15:32:09 +0100

    Read user command and quit

--- ruby/lib/user_interface/user_interface.rb
+++ ruby/lib/user_interface/user_interface.rb
@@ -10,30 +10,28 @@ module Ui
 			@catalogueActions = catalogueActions
 			@warehouseActions = warehouseActions
 			@basketActions = basketActions
-			# @in = BufferedReader.new(new InputStreamReader(input))
 		end
 
 		def start
-			# for (;;)
-			# 	System.out.print("shop> ")
-			# 	begin
-			# 		String line = in.readLine
-			# 		if (line == null)
-			# 			System.out.println
-			# 			return
-			# 		end
-			# 		line = line.trim
-			# 		if (line.equals(""))
-			# 			continue
-			# 		if (line.charAt(0) == 'q')
-			# 			return
-			# 		UserInput cmd = new UserInput(line)
-			# 		createHandler(cmd.command).run(cmd)
-			# 	end catch (IOException e)
-			# 	end catch (RuntimeException e)
-			# 		System.err.println("ERROR: " + e.getMessage)
-			# 	end
-			# end
+			while 1 do
+				print "shop> "
+				$stdout.flush
+				begin
+					String line = gets
+					if line.nil?
+						puts
+						return
+					end
+					line = line.strip
+					unless line.empty?
+						return if line[0] == 'q'
+						cmd = UserInput.new(line)
+						createHandler(cmd.command).run(cmd)
+					end
+				rescue Exception => e
+					$stderr.puts "ERROR: #{e}"
+				end
+			end
 		end
 
 		def createHandler(command)
