44b9be22e10683d2979b8c9eccde11b18cfd305b
Kevin Rutherford <kevin@rutherford-software.com>
2016-09-08 13:58:24 +0100

    Test for error messages when replenishing

---
+++ java/spec/default_stock_levels_spec.rb
@@ -0,0 +1,11 @@
+require_relative './spec_helper'
+
+describe('default stock levels') do
+
+  example('match those in the file') do
+    Shop.create do |shop|
+      expect(shop.send('s')).to eq(['24 17 5990       50', '10 04 2761       17', '01 01 6000      105', '01 12 1045        3'])
+    end
+  end
+
+end
