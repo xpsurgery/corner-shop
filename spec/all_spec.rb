f978f1efb22dedac84daeb1e3b516b3e23f7ce54
Kevin Rutherford <kevin@rutherford-software.com>
2016-09-09 12:17:02 +0100

    Test the ruby variant too

--- spec/all_spec.rb
+++ spec/all_spec.rb
@@ -3,16 +3,16 @@ require 'shared/basket'
 require 'shared/default_stock_levels'
 require 'shared/replenish_stock'
 
-[
+{
   # 'csharp',
-  'java',
-  # 'ruby'
-].each do |dir|
+  'java' => 'make run',
+  'ruby' => 'bin/shop.rb'
+}.each do |dir, cmd|
   Dir.chdir(dir) do
     describe(dir) do
-      include_examples 'basket', dir, 'make run'
-      include_examples 'default stock levels', dir, 'make run'
-      include_examples 'replenishing stock', dir, 'make run'
+      include_examples 'basket', dir, cmd
+      include_examples 'default stock levels', dir, cmd
+      include_examples 'replenishing stock', dir, cmd
     end
   end
 end
